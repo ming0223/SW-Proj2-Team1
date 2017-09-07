e99b04b727a3f829651004fa2c2729a8125675b7
HyunWoongAn <asd212164@kookmin.ac.kr>
2017-09-07 11:09:32 +0900

    Update 20171645-AnHyunWoong-assignment2.py

--- 20171645-AnHyunWoong-assignment2.py
+++ 20171645-AnHyunWoong-assignment2.py
@@ -3,6 +3,8 @@ while (True):
     num = int(input("Input Number: "))
     if(num == -1):
      break
+    elif(num < 0):
+     print("REType Number")
     else:
      for i in range (1, num+1):
       fac *= i
