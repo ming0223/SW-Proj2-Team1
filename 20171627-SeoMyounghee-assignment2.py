549f638e6fb3d5420f0796423859ac5d00f7fcdb
MyoungheeSeo <31427427+MyoungheeSeo@users.noreply.github.com>
2017-09-07 10:47:15 +0900

    Add files via upload

---
+++ 20171627-SeoMyounghee-assignment2.py
@@ -0,0 +1,15 @@
+num=0
+while num!= -1:
+    num= int(input("Enter a number:"))
+    if num==0:
+        print(str(num)+"!="+str(1))
+    elif num==-1:
+        break
+    elif num<=0:
+        print("다시 입력하세요")
+    else:
+        fac=1
+        for i in range(1, num+1):
+            fac=i*fac
+        print(str(num)+"!="+str(fac))
+
