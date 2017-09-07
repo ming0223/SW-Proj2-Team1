fefb6b80e432e1e96b31faffe7a8a5dce44f5521
MyoungheeSeo <31427427+MyoungheeSeo@users.noreply.github.com>
2017-09-07 11:09:16 +0900

    Update 20171627-SeoMyounghee-assignment2.py

--- 20171627-SeoMyounghee-assignment2.py
+++ 20171627-SeoMyounghee-assignment2.py
@@ -1,11 +1,11 @@
-while (True):
-    num= int(input("Enter a number:"))
-    if num==-1:
+while (True):   
+    num= int(input("Enter a number:"))      # 숫자입력받기
+    if num==-1:     #-1을 넣으면 프로그램이 꺼짐
         break
-    elif num<0:
+    elif num<0:        #음수일 때 예외처리
         print("다시 입력하세요")
-    else:
-        fac=1
-        for i in range(1, num+1):
+    else:       
+        fac=1       
+        for i in range(1, num+1):       #팩토리얼 계산 루프문
             fac=i*fac
         print(str(num)+"!="+str(fac))
