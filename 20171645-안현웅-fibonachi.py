24b0c8f70d6f3120027d7ddeb489cc276155deb1
HyunWoongAn <asd212164@kookmin.ac.kr>
2017-10-11 20:20:04 +0900

    Update 20171645-안현웅-fibonachi.py

--- 20171645-안현웅-fibonachi.py
+++ 20171645-안현웅-fibonachi.py
@@ -3,8 +3,8 @@ import time
 def fibonachi(n):
     if n == 1:
         return 0
-    elif n == 2:
-        return 1
+    elif n == 0:
+        return 0
     else:
         return fibonachi(n-1) + fibonachi(n-2)
 
