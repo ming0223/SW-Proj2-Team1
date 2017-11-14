87a2bcd06af2746abd3a12189d558b6ab5de08a5
MyoungheeSeo <31427427+MyoungheeSeo@users.noreply.github.com>
2017-11-14 11:14:42 +0900

    20171627-서명희-assignment11

---
+++ calcFunctions.py
@@ -0,0 +1,43 @@
+from math import factorial as fact
+
+def factorial(numStr):  #math 모듈이 아닌 for문을 이용한 계산
+    try:
+        num = int(eval(numStr))  # 숫자 정수로 변환    #eval 함수를 씀으로서 괄호안의 계산이 에러처리 되지 않고 계산 됨
+        if num < 0:
+            r='Error!'
+        else:
+            r = 1
+            for i in range(1, num + 1):  # 팩토리얼 계산 루프문
+                r = i * r
+    except:
+        r='Error!'
+    return str(r)
+
+    # try:
+    #      if numStr < 171:
+    #          n = int(eval(numStr))       #괄호 안의 수도 계산되게 수정
+    #          r = str(fact(n))
+    #      else:
+    #          r= 'infinite'
+    #  except:
+    #      r = 'Error!'
+    # return r
+
+def decToBin(numStr):
+    try:
+        n = int(eval(numStr))       #괄호 안의 수도 계산되게 수정
+        r = bin(n)[2:]
+    except:
+        r = 'Error!'
+    return r
+
+def binToDec(numStr):
+    try:
+        n = int(numStr, 2)
+        r = str(n)
+    except:
+        r = 'Error!'
+    return r
+
+def decToRoman(numStr):
+    return 'dec -> Roman'
