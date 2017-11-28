b6be8e926d97b4de8196c0e2c3d2ed3376540d8f
서형석 <ming0223@naver.com>
2017-11-28 01:40:09 +0000

    행맨 ver1

---
+++ 20171633_서형석_Hangman/guess.py
@@ -0,0 +1,52 @@
+class Guess:
+
+    def __init__(self, word):
+        self.secretWord = word
+        self.guessedChars = ""
+        self.numTries = 0
+        self.currentStatus = ""
+
+        for i in range(0, len(self.secretWord)) :
+            self.currentStatus += '_'
+
+    def display(self):
+
+        print("Current: %s" %self.currentStatus)
+        print("Tries: " + str(self.numTries))
+
+
+    def ChangeStr(self, character, place) :
+        str = ""
+
+        for i in range(0, len(self.currentStatus)) :
+            if(i in place) :
+                str += character
+            else :
+                str += self.currentStatus[i]
+
+        return str
+
+    def guess(self, character):
+#        try :
+        if character in self.secretWord :
+            count = 0
+            list = []
+            findPlace = self.secretWord.find(character, count)
+            while(findPlace != -1) :
+                findPlace = self.secretWord.find(character, count)
+                list.append(findPlace)
+                count = findPlace + 1
+
+            self.currentStatus = self.ChangeStr(character, list)
+
+        else :
+            self.numTries += 1
+#        except :
+#            print("Error!")
+
+        self.guessedChars += character
+
+        if(self.secretWord == self.currentStatus) :
+            return True
+        else :
+            return False
